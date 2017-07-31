6669bc1ff0e3054b05018f08fb7f350bc65344b1
Yoshiyuki Kuga <yossan4343434@gmai.com>
2017-07-31 11:44:05 +0900

    match function created

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -14,7 +14,12 @@ class MatchesController < ApplicationController
 
   # GET /matches/new
   def new
-    @match = Match.new
+    today = "2017-07-31"
+    standby_user_ids = Standby.where(date: today).pluck(:user_id).shuffle!
+
+    standby_user_ids.each_slice(2) do |uid1, uid2|
+      @match = Match.create(user1_id: uid1, user2_id: uid2, date: today)
+    end
   end
 
   # GET /matches/1/edit
@@ -70,5 +75,19 @@ class MatchesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def match_params
       params.require(:match).permit(:user1_id, :user2_id, :date)
+    end
+
+    def start_match
+      @match = Match.new(match_params)
+
+      respond_to do |format|
+        if @match.save
+          format.html { redirect_to @match, notice: 'Match was successfully created.' }
+          format.json { render :show, status: :created, location: @match }
+        else
+          format.html { render :new }
+          format.json { render json: @match.errors, status: :unprocessable_entity }
+        end
+      end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :standbies
+  has_many :matches
 end
